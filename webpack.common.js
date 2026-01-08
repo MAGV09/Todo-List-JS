534b8f239c1eceac1e27ad4ed0bb22330946ff69
MAGV09 <110742063+MAGV09@users.noreply.github.com>
2026-01-08 15:21:21 +0200

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/script.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      //   {
+      //     test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      //     type: 'asset/resource',
+      //   },
+    ],
+  },
+};
